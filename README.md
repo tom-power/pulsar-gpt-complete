3ca80c4549df16b3fd2b5d6ee8e9a258b1daef8c
Tom Power <tpower21@gmail.com>
2023-06-23 13:10:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ChatGPT Completions in Pulsar
+
+Simple package to call OpenAI [completions](https://api.openai.com/v1/chat/completions) in Pulsar.
+
+Roles/prompts borrowed from [ShellGpt](https://github.com/TheR1D/shell_gpt) using `gpt-3.5-turbo`.
+
+# Commands
+
+| Command                               | Description                            |
+| ------------------------------------- | -------------------------------------- |
+| pulsar-gpt-complete:selection to code | output code completion below selection |
---
+++ spec/pulsar-gpt-complete-spec.js
@@ -0,0 +1,55 @@
+const {
+  getPulsarGptComplete
+} = require('../lib/lib/pulsar-gpt-complete');
+
+const pulsarGptComplete = getPulsarGptComplete()
+
+describe("pulsar gpt complete", () => {
+  beforeEach(async () => {
+    pulsarGptComplete.activate()
+  })
+
+  afterEach(async () => {
+    pulsarGptComplete.deactivate()
+  })
+
+  describe("pulsar gpt complete integration", () => {
+    let editor = null;
+    let editorView = null;
+
+    beforeEach(() => {
+      waitsForPromise(() => atom.workspace.open('test.txt'));
+      waitsForPromise(() => atom.packages.activatePackage('pulsar-gpt-complete'));
+
+      return runs(() => {
+        editor = atom.workspace.getActiveTextEditor();
+        editorView = atom.views.getView(editor);
+      });
+    });
+
+    describe("selection to code", () => {
+      it("should generate code from selection", () => {
+        expectedPrompt = "// a commented prompt"
+        expectedCode = "let generated=code"
+        expectedCodeData = {
+          data: {
+            choices: [{
+              message: {
+                content: expectedCode
+              }
+            }]
+          }
+        };
+        spyOn(pulsarGptComplete, 'selectionToCodeCompletion').andReturn(Promise.resolve(expectedCodeData));
+
+        expect(editor.getText()).toBe(expectedPrompt + "\n");
+
+        selectionToCode = atom.commands.dispatch(editorView, "pulsar-gpt-complete:selection-to-code")
+
+        selectionToCode.then(() =>
+          expect(editor.getText()).toBe(expectedPrompt + "\n" + expectedCode + "\n")
+        );
+      });
+    });
+  });
+});
