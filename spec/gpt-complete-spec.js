fdeffbc7d1ca06e74a366364a264400ca3f1fa9b
Tom Power <tp21@tuta.io>
2023-06-29 08:08:42 +0100

    don't request an output language when there's no grammar

--- spec/gpt-complete-spec.js
+++ spec/gpt-complete-spec.js
@@ -3,47 +3,60 @@ const dedent = require('dedent-js');
 
 const gptComplete = new GptComplete("testKey");
 
+const testRole = {
+  name: "testName",
+  description: "testDescription",
+  expecting: "testExpecting"
+}
+
+function expectedContent(outputRequest) {
+  return `###
+          Role name: testName
+          testDescription${outputRequest}
+
+          Request: testRequest
+          ###
+          testExpecting:`
+}
+
+function expectedRequest(outputRequest) {
+  return {
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: "user",
+      content: dedent(expectedContent(outputRequest))
+    }]
+  }
+}
+
 describe("gpt complete", () => {
   it("should pass correct request to openai", () => {
-
     spyOn(gptComplete.openai, 'createChatCompletion')
-
-    const testRole = {
-      name: "testName",
-      description: "testDescription",
-      expecting: "testExpecting"
-    }
 
     gptComplete.completionFor("testRequest", testRole, "testGrammar");
 
-    function expectedContent() {
-      return `###
-              Role name: testName
-              testDescription 
-              Output should be in testGrammar.
+    actualRequest = gptComplete.openai.createChatCompletion.calls[0].args[0]
 
-              Request: testRequest
-              ###
-              testExpecting:`
-    }
+    expect(actualRequest).toEqual(expectedRequest("\nOutput should be in testGrammar"))
+  });
 
+  it("shouldn't request an output language when there's a null grammar", () => {
+    spyOn(gptComplete.openai, 'createChatCompletion')
 
-    expectedRequest = {
-      model: "gpt-3.5-turbo",
-      messages: [{
-        role: "user",
-        content: dedent(expectedContent())
-      }]
-    }
+    gptComplete.completionFor("testRequest", testRole, "text.plain.null-grammar");
 
     actualRequest = gptComplete.openai.createChatCompletion.calls[0].args[0]
 
-    expect(actualRequest.model).toEqual(expectedRequest.model)
+    expect(actualRequest).toEqual(expectedRequest(""))
+  });
 
-    expectedMessage = expectedRequest.messages[0]
-    actualMessage = actualRequest.messages[0]
+  it("shouldn't request an output language when there's no grammar", () => {
+    spyOn(gptComplete.openai, 'createChatCompletion')
 
-    expect(actualMessage.role).toEqual(expectedMessage.role)
-    expect(actualMessage.content).toEqual(expectedMessage.content)
+    gptComplete.completionFor("testRequest", testRole, "");
+
+    actualRequest = gptComplete.openai.createChatCompletion.calls[0].args[0]
+
+    expect(actualRequest).toEqual(expectedRequest(""))
   });
 });
